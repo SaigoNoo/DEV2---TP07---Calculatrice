b682b3bc0474304c4f363cd0abbb901b22e5a2e3
Giorgios Doussis <giorgiodoussis@gmail.com>
2023-11-29 23:12:34 +0100

    #firstRelease #tp7
    Trop long a écrire, 😴

---
+++ math_custom/__init__.py
@@ -0,0 +1,7 @@
+"""
+Ceci sert juste a centraliser les imports.
+De plus, ca simplifie les appels de classe !
+"""
+
+from math_custom.simplifier import Simplifier
+from math_custom.calcul import Calcul
